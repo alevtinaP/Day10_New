5e47a0982d43a49705b315a010e02686f38ce218
Alevtina Petrova <alevtina.piatrova@gmail.com>
2026-06-23 11:13:10 +0300

    Созданы проверки работоспособости приложения (создание, изменение и вывод статистики)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,25 @@
 public class Main {
+    static void main(String[] args) {
+        TaskTracker tracker = new TaskTracker();
+
+//добавление задач
+        tracker.addTask(" Проснуться ");
+        tracker.addTask(" Сделать кофе ");
+        tracker.addTask(" Выпить кофе ");
+        tracker.addTask(" Включить комп ");
+        tracker.addTask(" Проверить почту ");
+
+        //вывод всех задач
+        tracker.printAllTask();
+
+        //проверка статуса
+        tracker.checkTaskStatus(" Проснуться ");
+
+        //изменение статуса
+        tracker.completeTask(" Выпить кофе ");
+
+        tracker.printAllTask();
+        tracker.printStatistic();
+
+    }
 }
