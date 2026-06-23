449c030b008beb3992d27d95653887f1f2fae3a8
Alevtina Petrova <alevtina.piatrova@gmail.com>
2026-06-23 11:07:04 +0300

    Созданы методы класса Task (получение статуса задачи, получение имени задачи, вывод информации и конструктор класса)

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,31 @@
 public class Task {
+    private String title;
+    private boolean isCompleted;
+
+    //Создаем конструктор класса
+    public Task(String title) {
+        this.title = title;
+        this.isCompleted = false;
+    }
+
+    //метод помечает задачу как выполнена
+    public void complete() {
+        this.isCompleted = true;
+    }
+
+    //метод получает статус задачи
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    //метод получает название задачи
+    public String getTitle() {
+        return title;
+    }
+
+    //Выводим информацию о задаче
+    public void printInfo() {
+        String status = isCompleted ? "[x]" : "[ ]";
+        System.out.println(status + " " + title);
+    }
 }
